7f04d088d5e54ce203768fcbc16b2886f362a415
hannahklecan <59770688+hannahklecan@users.noreply.github.com>
2021-10-08 15:56:53 -0700

    Update ParticleLabFiles.java

--- ParticleLabFiles.java
+++ ParticleLabFiles.java
@@ -1,20 +1,20 @@
 /*********************************************************************************************************************
-  * SandLabFiles.java
-  *********************************************************************************************************************/
+ * SandLabFiles.java
+ *********************************************************************************************************************/
 import java.io.*;
 import java.util.*;
 
 public class ParticleLabFiles {
     static FileWriter  fw;
     static PrintWriter pw;
-    
+
     static FileReader fr;
     static BufferedReader br;
-    
+
     static int nbrOfRows = ParticleLab.getNbrRows();  //160;
     static int nbrOfCols = ParticleLab.getNbrCols();  //180;
-    
-    
+
+
     public static void main(String[] args) throws IOException {
         System.out.println("Executing ParticleLabFiles.main directly is not recommended.");
         if (args.length > 0){
@@ -23,15 +23,15 @@ public class ParticleLabFiles {
             readFile("ParticleLabFile.txt");
         }
     }
-    
+
     public static void writeFile(int [][] sandLabArray, String fileName)  {
         System.out.println("\nStarting ParticleLabFiles.writeFile()");
         System.out.println("    Writing: " + fileName);
-                                                      
+
         try {
             fw = new FileWriter(fileName, false);
             pw = new PrintWriter(fw);
-            
+
             for (int r = 0; r < nbrOfRows; r++) {
                 for (int c = 0; c < nbrOfCols; c++) {
                     pw.print(sandLabArray[r][c] + " ");
@@ -45,38 +45,38 @@ public class ParticleLabFiles {
         catch (Exception e) {
             System.err.println("Error writing file. Ending program.");
             System.exit(-1);
-        }  
-    } 
-      
-    public static void setRowsAndCols(int rows, int cols){
-        nbrOfRows = rows; 
-        nbrOfCols = cols; 
+        }
     }
-    
+
+    public static void setRowsAndCols(int rows, int cols){
+        nbrOfRows = rows;
+        nbrOfCols = cols;
+    }
+
     //////////////////////////////////////////////////////////////////////////////////////////
-    
+
     public static int[][] readFile(String fileName) {
         System.out.println("Finding: " + fileName);
-        int [][] grid = new int[nbrOfRows][nbrOfCols]; 
+        int [][] grid = new int[nbrOfRows][nbrOfCols];
         String value = "";
-        
-        try { 
-            
+
+        try {
+
             File file = new File(fileName);
             Scanner in = new Scanner(file);
-            
+
             for (int r = 0; r < nbrOfRows; r++) {
                 for (int c = 0; c < nbrOfCols; c++) {
                     grid[r][c] = in.nextInt();
                 }
             }
-            
+
         }
         catch (Exception e) {
             System.err.println("Error reading file. Has the size changed? Does it exist?");
             System.err.println("Ending program.");
             System.exit(-1);
-        } 
-        return grid;      
+        }
+        return grid;
     }
 }
